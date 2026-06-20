9d11f4ca852c2618b14f8c7b2e3adf0f14fd30f1
Nabila Melsyana <nabilamelsyana5@gmail.com>
2026-06-20 21:03:22 +0700

    refactor: rename entry point file to index.html and update routing configuration

--- server.js
+++ server.js
@@ -75,7 +75,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (_req, res) => res.sendFile(path.join(__dirname, 'public', 'academy_quiz.html')));
+app.get('/', (_req, res) => res.sendFile(path.join(__dirname, 'public', 'index.html')));
 
 app.get('/api/health', async (_req, res) => {
   try { await pool.query('SELECT 1'); res.json({ ok: true, db: true }); }
